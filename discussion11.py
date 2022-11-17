5bb7a93e768c5620ac3ab4f4cbcababeeb34a447
Emily Vanek <emilyvanek@Emilys-MacBook-Air-40.local>
2022-11-16 21:20:35 -0500

    discussion

--- discussion11.py
+++ discussion11.py
@@ -39,12 +39,16 @@ def create_species_table(cur, conn):
 # TASK 1
 # CREATE TABLE FOR PATIENTS IN DATABASE
 def create_patients_table(cur, conn):
-    pass
+    cur.execute("DROP TABLE IF EXISTS Patients")
+    cur.execute("CREATE TABLE Patients (pet_id INTEGER PRIMARY KEY, name TEXT, species_id NUMBER, age INTEGER, cuteness INTEGER, aggressiveness NUMBER)") 
+    conn.commit()
 
 
 # ADD FLUFFLE TO THE TABLE
 def add_fluffle(cur, conn):
-    pass
+    cur.execute('insert into Patients (pet_id, name, species_id, age, cuteness, aggressiveness values(?,?,?,?,?,?', (0,"Fluffle", 0, 3, 90, 100))
+    conn.commit()
+
     
 
 # TASK 2
